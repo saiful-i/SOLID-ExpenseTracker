b73c3cf0f64da107afc3cb4ab92981ea6675a308
saiful <mzuquare@gmail.com>
2023-03-17 00:14:18 +0700

    Adding Weekly Reports

--- ExpenseTracker/Enum/ReportRange.swift
+++ ExpenseTracker/Enum/ReportRange.swift
@@ -34,6 +34,7 @@ import Foundation
 enum ReportRange: String, CaseIterable {
   case daily = "Today"
   case monthly = "This Month"
+  case weekly = "This Week"
   
   func timeRange() -> (Date, Date) {
     let now = Date()
@@ -42,6 +43,8 @@ enum ReportRange: String, CaseIterable {
       return (now.startOfDay, now.endOfDay)
     case .monthly:
       return (now.startOfMonth, now.endOfMonth)
+    case .weekly:
+      return (now.startOfWeek, now.endOfWeek)
     }
   }
 }
