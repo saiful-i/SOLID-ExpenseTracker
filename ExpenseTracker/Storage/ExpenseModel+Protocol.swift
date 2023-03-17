d7403a006a94dd9ca8bf116d4b642f0b5d2e83c8
saiful <mzuquare@gmail.com>
2023-03-17 14:09:53 +0700

    Applying Dependency Inversion

--- ExpenseTracker/Storage/ExpenseModel+Protocol.swift
+++ ExpenseTracker/Storage/ExpenseModel+Protocol.swift
@@ -1,4 +1,4 @@
-/// Copyright (c) 2021 Razeware LLC
+/// Copyright (c) 2023 Razeware LLC
 /// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -30,60 +30,6 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import CoreData
-import Combine
 
-class ReportsDataSource: ReportReader {
-  let reportRange: ReportRange
-  var viewContext: NSManagedObjectContext
-
-  init(viewContext: NSManagedObjectContext = PersistenceController.shared.container.viewContext, reportRange: ReportRange) {
-    self.viewContext = viewContext
-    self.reportRange = reportRange
-    super.init()
-    prepare()
-  }
-
-  override func prepare() {
-    currentEntries = getEntries()
-  }
-
-  private func getEntries() -> [ExpenseModelProtocol] {
-    let fetchRequest: NSFetchRequest<ExpenseModel> = ExpenseModel.fetchRequest()
-    fetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \ExpenseModel.date, ascending: false)]
-    let (startDate, endDate) = reportRange.timeRange()
-    fetchRequest.predicate = NSPredicate(
-      format: "%@ <= date AND date <= %@",
-      startDate as CVarArg,
-      endDate as CVarArg)
-    do {
-      let results = try viewContext.fetch(fetchRequest)
-      return results
-    } catch let error {
-      print(error)
-      return []
-    }
-  }
-
-  override func saveEntry(title: String, price: Double, date: Date, comment: String) {
-    let newItem = ExpenseModel(context: viewContext)
-    newItem.title = title
-    newItem.date = date
-    newItem.comment = comment
-    newItem.price = price
-    newItem.id = UUID()
-
-    if let index = currentEntries.firstIndex(where: { $0.date ?? Date() < date }) {
-      currentEntries.insert(newItem, at: index)
-    } else {
-      currentEntries.append(newItem)
-    }
-
-    try? viewContext.save()
-  }
-
-  func delete(entry: ExpenseModel) {
-    viewContext.delete(entry)
-    try? viewContext.save()
-  }
+extension ExpenseModel: ExpenseModelProtocol {
 }
